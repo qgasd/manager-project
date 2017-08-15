6cdaea6ff1614f968107b9c158453c1d22de18a5
kubasen007 <732855835@qq.com>
2017-08-15 14:18:55 +0800

    修改接口调用关系

--- manager-server/routes/interfaceRefrenceController.js
+++ manager-server/routes/interfaceRefrenceController.js
@@ -25,24 +25,54 @@ var db = require('../public/javascripts/mysql.js');
 router.get("/serach/:id",function(req,res,next){
     var num = req.params.id;
         //db.query("SELECT * FROM int_invoke_relation WHERE int_service_num="+num,function(err,rows){
-         db.query("SELECT * FROM int_invoke_relation WHERE FIND_IN_SET(invoke_int_service_num,getParLst ("+num+")) ",function(err,rows){
-         if(err){
-             console.log("查询错误："+err);
-         }else {
-             console.log(JSON.stringify(rows));
-             if(rows.length>0){
-                var str = JSON.stringify(convert(rows,num));
-                console.log("str:"+str);
-                if(str!='undefined')
-                var jsonstr =  '{"int_service_num":'+num+","+str.substring(1,str.length);
-                console.log("jsonstr:"+jsonstr);            
-                res.send(jsonstr);
-             }else{
-                 var str = '{"int_service_num":'+num+'}'
-                res.send(str);
-             }
-            
-         }
+         //db.query("SELECT * FROM int_invoke_relation WHERE FIND_IN_SET(invoke_int_service_num,getParLst ("+num+")) ",function(err,rows){
+            db.query(
+            `SELECT 
+                a.*,b.* 
+            FROM
+                int_invoke_relation a,int_information b  
+            WHERE FIND_IN_SET(invoke_int_service_num, getParLst (`+num+`))
+            AND a.int_service_num=b.int_service_num ;`,
+          function(err,rows){
+            if(err){
+                console.log("查询错误："+err);
+            }else {
+                db.query(`
+                    SELECT 
+                        * 
+                    FROM
+                        int_information b 
+                    WHERE b.int_service_num=`+num+`
+                `,function(err,rows2){
+                    if(err){
+                        console.log("查询错误："+err);
+                    }else{
+                        console.log(JSON.stringify(rows));
+                        console.log(JSON.stringify(rows2));
+                        if(rows.length>0){
+                            var str = JSON.stringify(convert(rows,num));
+                            console.log("str:"+str);
+                            if(str!='undefined')
+                            var jsonstr =  '{"int_name_cn":"'+rows2[0].int_name_cn
+                            +'","int_name_en":"'+rows2[0].int_name_en
+                            +'","description":"'+rows2[0].description+'","int_service_num":'+num+','
+                            +str.substring(1,str.length);
+                            console.log("jsonstr:"+jsonstr);            
+                            res.send(jsonstr);
+                        }else{
+                            var str = '{"int_name_cn":"'+rows2[0].int_name_cn
+                            +'","int_name_en":"'+rows2[0].int_name_en
+                            +'","description":"'+rows2[0].description+'","int_service_num":'+num+'}'
+                            console.log("s:"+str);
+                            res.send(str);
+                        }
+                    }
+                    
+
+                })
+               
+
+            }
      });
  });
  
@@ -58,7 +88,9 @@ router.get("/serach/:id",function(req,res,next){
          if(!tmp[item.int_service_num]){
              tmp[item.int_service_num]={};
          }
-        // tmp[item.int_service_num].Name=item.Name;
+         tmp[item.int_service_num].int_name_cn=item.int_name_cn;
+         tmp[item.int_service_num].int_name_en=item.int_name_en;
+         tmp[item.int_service_num].description=item.description;
          tmp[item.int_service_num].int_service_num=item.int_service_num;
          if(!("children" in tmp[item.int_service_num]))tmp[item.int_service_num].children=[];
           
