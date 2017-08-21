ded4fdd8772d6b34e9f514c7e7a7b67e2cf76e2f
AilsaGreen <572084074@qq.com>
2017-08-21 23:19:18 +0800

    AilsaGreen
    页面更新

--- manager/src/app/dashboard/interface/interface.component.ts
+++ manager/src/app/dashboard/interface/interface.component.ts
@@ -1,210 +1,318 @@
-import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { ofInterface } from './interface-mock-data/mock-data';
 import { InterfaceService } from './interface-service/interface-service';
 import { FormsModule } from '@angular/forms';
-import { Subject }     from 'rxjs/Subject';
-import { Observable }  from 'rxjs/Observable';
-import { Location }    from '@angular/common';
+import { Subject } from 'rxjs/Subject';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Location } from '@angular/common';
+import { selectDatasService } from "app/common-service/select-service";
+// import {MultiSelectModule} from 'primeng/primeng';
+import { ConfirmationService } from 'primeng/primeng';
+declare var $: any;
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
-  styleUrls: ['./interface.component.css']
+  styleUrls: ['./interface.component.css'],
+  providers: [ConfirmationService]
 })
 export class InterfaceComponent implements OnInit {
-  public interdatas:ofInterface[];//通用的数组
-  public dedata:ofInterface;//shanc
-  public cc:ofInterface[];
-  public filterDatas:ofInterface[];
+  public interdatas: ofInterface[];//通用的数组
+  public deletedatas: ofInterface[];
+  public cc: ofInterface[];
+  public filterDatas: ofInterface[];
   public zbdatas: ofInterface[];//这是多条件查询的数据
- // public cdatas:Observable<ofInterface[]>;//这是单条件搜索的数据
-  public cdatass:ofInterface[];//这是单条件搜索的数据
+  // public cdatas:Observable<ofInterface[]>;//这是单条件搜索的数据
+  public cdatass: ofInterface[];//这是单条件搜索的数据
   public flag2 = false;//状态码
   public flag1 = true;
-  public flag4 =false;
-  public flag5 = false;
+  //public flag6=false;
+  public flag4 = false;//pc和移动端字段是否显示的状态，初始值都显示，在页面的宽度小于750的时候显示三个字段
+  public flag5 = false;//复选框选中状态
   public seardata: ofInterface[];//获取全部的数据
   public selId: ofInterface[];//选中相对应的id
-private searchTerms = new Subject<string>();//获取subject对象
-public jkfwdata:string;
-public flag3=false;//这是单条件查询的
-public choseItem=[];
-public chdates:ofInterface[];
-/**
- * 分页参数
- */
+  private searchTerms = new Subject<string>();//获取subject对象
+  public jkfwdata: string;
+  isChecked = false;
+  public aradata: any;
+  public checkBox: boolean;
+  public checkBoxClick: boolean;
+  public flag3 = false;//这是单条件查询的
+  public choseItem = [];//设置一个空数组，将选中的数据放到里面传给后台判断是否删除
+  public flag0 = true;
+  public booleanArray = [];
+  public indexw: number;//编辑input显示和不显示
+  public deletdata: ofInterface[];
 
-  public totalItems: number =  this.totalItems; //所有数据
-  public maxSize:number = 10;
+  public flag6 = true;
+  public flag7 = true;
+  public flag8 = true;
+  public flag9 = true;
+  public flag10 = false;
+  /**
+   * 分页参数
+   */
+  public totalItems: number = this.totalItems; //所有数据
+  public maxSize: number = 10;
   public currentPage: number = 1;//当前页
   public smallnumPages: number = 0;
-  public itemsPerPage:number = 10;//当前选择10条一页
-  public eventData:any;
-  public choosedata:ofInterface;
-//实现对象的观察1
-  constructor(public interfaceservice: InterfaceService,public location:Location) { 
+  public itemsPerPage: number = 4;//当前选择10条一页
+  public eventData: any;
+  public choosedata: ofInterface;
+  //实现对象的观察1
+  innerWidth: number;
+  //select
+  public selectstate: ofInterface;
+  //弹窗标题
+  public alertTitle: string;
+  //弹窗状态码
+  public alertSearch = false;
+  public alertEdit = false;
+  public alertAdd = false;
+  //,private cdr: ChangeDetectorRef  ,private window: Window
+  constructor(public interfaceservice: InterfaceService, public location: Location, public sservice: selectDatasService, public confirmationService: ConfirmationService,public router:Router) {
+    //  let getWindow = () => {
+    //       return window.innerWidth;
+    //    };
 
+    //   window.onresize = () => {
+    //       this.innerWidth = getWindow();
+    //       this.cdr.detectChanges(); //running change detection manually
+    //   };
   }
-//获取页面的元素
-@ViewChild('checkvalue')
-checkVal:ElementRef
-/**
- * 需不需要判断当单选框的值为空的时候主页面显示哪个数据，感觉有点问题
- */
-@HostListener('window:resize', ['$event'])
+  //获取页面的元素
+  @ViewChild('checkvalue')
+  checkVal: ElementRef
+  /**
+   * 需不需要判断当单选框的值为空的时候主页面显示哪个数据，感觉有点问题
+   */
+  @HostListener('window:resize', ['$event'])
 
-onResize(event) {
-  console.log(event)
-  var wwinth=event.target.innerWidth;
+  onResize(event) {
+    console.log(event)
+    var wwinth = event.target.innerWidth;
     console.log(wwinth)
-      if(wwinth<750){
-          this.flag4 = false;
-      }else{
-        this.flag4 = true;
-      }
-}
-
-  searchInter(jkfw: string) {
-    this.jkfwdata = jkfw;
-    this.interfaceservice.searchIn(jkfw).subscribe(res=>{
-      this.cdatass=res['items'];
-      this.totalItems = res['total']//this.interdatas.length;
-     if(jkfw==undefined || jkfw ==""){
-       //  this.loadData();
-       }//设置这边的当输入框中的内容为空的时候全部数据出发      
-       if(this.cdatass && this.seardata){
-       this.interdatas = this.cdatass;
-    //   this.interdatas=this.interdatas.filter(h=>h.state!=='hidden')
-       }
-    }) 
-   this.flag3 =!this.flag3;
-  }
-   /**
- *多条件查询
- */
-  advanceSearch(cc: ofInterface) {
-    this.choosedata = cc;
-    this.interfaceservice.gadsearch(cc).subscribe(res=>{
-      this.zbdatas =res['items'];
-     if(this.zbdatas && this.seardata || this.cdatass){
-       this.interdatas = this.zbdatas;
-      // this.interdatas=this.interdatas.filter(h=>h.state!=='hidden')
-       this.totalItems =res['total']  //this.interdatas.length;
-      console.log(this.interdatas)
+    if (wwinth < 768) {
+      this.flag4 = false;
+      this.flag6 = true;
+      this.flag7 = false;
+      this.flag8 = true;
+      this.flag9 = false;
+      this.flag10 = false;
+    } else {
+      this.flag4 = true;
+      this.flag6 = false;
+      this.flag7 = true;
+      this.flag8 = true;
+      this.flag9 = true;
+      this.flag10 = false;
     }
-    
-      console.log(this.totalItems)});
-        this.flag1 =!this.flag1;
+  }
+
+  /**
+  *屏幕宽度识别导航
+  */
+  screen() {
+    var wwinth = window.innerWidth;
+    if (wwinth < 768) {
+      this.flag4 = false;
+      this.flag6 = true;
+      this.flag7 = false;
+      this.flag8 = true;
+      this.flag9 = false;
+      this.flag10 = false;
+    } else {
+      this.flag4 = true;
+      this.flag6 = false;
+      this.flag7 = true;
+      this.flag8 = true;
+      this.flag9 = true;
+      this.flag10 = false;
+    }
+  }
+  /**
+     * 取消全选
+     */
+  cancel() {
+    var j_cbAllinput = $("#j_cbAll input");
+    j_cbAllinput[0].checked = false;
+  }
+  /**
+   * 全选
+   */
+  choseALL(cb: ofInterface, c: boolean) {
+    var checkboxs = $(".j_tbc input");
+    for (var i = 0; i < checkboxs.length; i++) {
+      var checkbox = checkboxs[i];
+      checkbox.checked = c;
+    }
+  }
+
+  /***
+   * 展开搜索
+   */
+  opensearch() {
+    this.flag6 = false;
+    this.flag7 = true;
+    this.flag8 = false;
+    this.flag9 = true;
+    this.flag10 = true;
+  }
+  /***
+* 返回搜索
+*/
+  backsearch() {
+    this.flag6 = true;
+    this.flag7 = false;
+    this.flag8 = true;
+    this.flag9 = false;
+    this.flag10 = false;
+  }
+  //获取select数组数据
+  //  public getselctDate(){
+  //     this.sservice.getSelectdata()
+  //     .subscribe(
+  //       res=>{
+  //           this.selectstate =res["state"];
+  //       },error=>{console.log(error)},
+  //     ()=>{}
+  //     );
+  //   }
+  //单条件搜索
+  searchInter(jkfw: string) {
+    this.cancel();
+    $(".waiting").show();
+    this.jkfwdata = jkfw;
+    this.interfaceservice.searchIn(jkfw).subscribe(res => {
+      $(".waiting").hide();
+      this.cdatass = res['items'];
+      this.totalItems = res['total']//this.interdatas.length;
+      if (jkfw == undefined || jkfw == "") {
+        this.loadData();
+      }//设置这边的当输入框中的内容为空的时候全部数据出发      
+      if (this.cdatass && this.seardata) {
+        this.interdatas = this.cdatass;
+        //   this.interdatas=this.interdatas.filter(h=>h.state!=='hidden')
+      }
+    })
+    this.flag3 = !this.flag3;
+  }
+  /**
+*多条件查询
+*/
+  advanceSearch(cc: ofInterface) {
+    $(".waiting").show();
+    console.log(this.alertSearch)
+    this.alertSearch = true;
+    this.alertEdit = false;
+    this.alertAdd = false;
+    this.getAlertTitle();
+    console.log(this.alertSearch)
+    this.choosedata = cc;
+    this.interfaceservice.gadsearch(cc).subscribe(res => {
+      this.zbdatas = res['items'];
+      if (this.zbdatas && this.seardata || this.cdatass) {
+        this.interdatas = this.zbdatas;
+        // this.interdatas=this.interdatas.filter(h=>h.state!=='hidden')
+        this.totalItems = res['total']  //this.interdatas.length;
+        console.log(this.interdatas)
+      }
+      console.log(this.totalItems)
+      $(".waiting").hide();
+    });
+    this.flag1 = !this.flag1;
   }
   /**
    *初始化实现的方法
    */
-  ngOnInit():void {
- 
-    this.getALLdata();     
-    //this.loadData();
-     //  console.log(this.interdatas)
-    //console.log(this.checkVal.nativeElement.style.text) 
-   
+  ngOnInit(): void {
+    this.getALLdata();
+    this.loadData();
+    this.getAlertTitle();
+    this.screen();
   }
-   /**
-   * 过滤禁用状态的数据
-   */
-
+  changeSearch() {
+     this.alertSearch=true;
+    this.alertAdd=false;
+    this.alertEdit=false;
+    this.alertTitle='搜索'
+  }
+  //
+  changeAdd(){
+      this.alertSearch=false;
+    this.alertAdd=true;
+    this.alertEdit=false;
+    this.alertTitle='新增'
+  }
+  
+  getAlertTitle() {
+    if (this.alertAdd) {
+      this.alertTitle = "新增"
+    } else if (this.alertEdit) {
+      this.alertTitle = "编辑"
+    } else if (this.alertSearch) {
+      console.log(this.alertSearch)
+      this.alertTitle = "搜索"
+    }
+    console.log(this.alertAdd)
+    console.log(this.alertEdit)
+    console.log(this.alertSearch)
+  }
   /**
  *获取全部的东西
  */
-  getALLdata() {    
-     let offset = (this.currentPage-1)*this.itemsPerPage;//分页中的开头
-		let end = (this.currentPage)*this.itemsPerPage;//分页中的结尾
-    //   if(this.flag1){
-          this.interfaceservice.getSdate().subscribe(res => {
-                this.seardata = res['items'].slice(offset,end>this.totalItems?this.totalItems:end);
-                this.interdatas = this.seardata;//隐藏状态被标记为禁用的数据
-               // this.interdatas= this.interdatas.filter(h=>h.state !=='hidden') ;
-                this.totalItems = res['total'];
-                console.log(34324)
-              }, error => { console.log(error) },
-                () => { }
-              )
-    //   }
-  }
-
- /**
- *删除
- */
-//  deleteInt(itemdata: ofInterface):void { 
-//     this.interfaceservice.deleteInt(itemdata.int_service_num).then(()=>
-//      this.seardata = this.seardata.filter(h => h !== itemdata)
-//     )
-//     location.reload();
-// }
-
-choseBox(item){
-   this.choseItem = item;
-   console.log(this.choseItem)
-}
-
- /**
- *新增
- */
-addSave(addDatas:ofInterface):void{
-   // console.log(this.Interfaces.push(addDatas))
-  
-  if(!addDatas){return}
-   if(addDatas.URL==undefined){
-     addDatas.URL="";
-   }
-    if(addDatas.remark==undefined){
-     addDatas.remark="";
-   }
-    this.interfaceservice.AddDatas(addDatas)
-   .then(ofInterface=>{this.seardata.push(ofInterface);location.reload();console.log(addDatas),console.log(this.interdatas)})
-}
-/**
- * 编辑里面的更新事件
- */
-EditUpdatec(SSdatas:ofInterface){
-    this.interfaceservice.EditUpdate(SSdatas).then( 
-        ()=>{this.goBack(),location.reload()}         
+  getALLdata() {
+    $(".waiting").show();
+    console.log(this.alertTitle)
+    this.interfaceservice.getSdate().subscribe(res => {
+      this.seardata = res['items']//.slice(offset, end > this.totalItems ? this.totalItems : end);
+      this.interdatas = this.seardata;//隐藏状态被标记为禁用的数据
+      this.totalItems = res['total'];
+      $(".waiting").hide();
+    }, error => {  $(".waiting").hide();alert("网络错误,请稍后重试！"); },
+      () => { }
     )
-   this.flag2=false;
-}
-/**
- * 返回
- */
-goBack():void{
-  this.location.back();
-}
-/**
- * 表示编辑的时候input框出现
- */
-editCon(){
-  this.flag2 = true;
-  console.log(this.flag2)
-}
-  /*
-  批量删除2
-  这个方法是获取说有的选中的数据并且将他们作为一个数组
-  */
-  deleteCheck(check: boolean, cc: ofInterface) {
-    // this.isChecked = cc.isChecked;
-    // //先判断选中的数组里面是否包括当前值,includes目前不支持
-    var index: number = this.choseItem.indexOf(cc.int_service_num)//首次出现的位置
-    ////当前选择的就追加否则就移除
-     this.ArrayWay(check,cc);
-     console.log(check)
-    console.log(this.choseItem)
-    //如果选中的参数长度等于当前每一页显示的数量那么就设置全选的状态为真 
   }
-
   /**
-   * 数组去重
+  *删除
+  */
+  // deleteInt(itemdata: ofInterface): void {
+  //   $(".waiting").show();
+  //   this.interfaceservice.deleteInt(itemdata.int_service_num).then(() =>
+  //     this.seardata = this.seardata.filter(h => h !== itemdata)
+  //   )
+  // }
+    /**
+   * 单选删除
+   */
+  deleteCheck(cc: ofInterface,check: boolean) {
+    var j_cbAll = $("#j_cbAll");
+    var j_cbAllinput = $("#j_cbAll input");
+    var checkboxs = $(".j_tbc input");
+    var isCheckedAll = true;
+    for (var i = 0; i < checkboxs.length; i++) {
+      if (!checkboxs[i].checked) {
+        isCheckedAll = false;
+        break;
+      }
+    }
+    if (isCheckedAll == true) {
+      j_cbAllinput[0].checked = true;
+    } else {
+      j_cbAllinput[0].checked = false;
+    }
+      this.ArrayWay(check, cc);
+      console.log(this.choseItem)
+  }
+  
+  /**
+   * 传给后台的数据 数组去重
    */
   ArrayWay(check: boolean, aw: ofInterface) {
-     var index: number = this.choseItem.indexOf(aw.int_service_num)
+    var index: number = this.choseItem.indexOf(aw.int_service_num)
     if (check) {
       if (index < 0) {
-        this.choseItem.push(aw.int_service_num)
+        this.choseItem.push(aw.int_service_num);
         console.log(this.choseItem)
       }
     } else {
@@ -216,69 +324,115 @@ editCon(){
     }
   }
 
-deleteCC(cc: ofInterface[]) {
-  console.log(cc)//cc是所有的数据
-  console.log(this.choseItem)
-  cc.forEach((el,index)=>{this.dedata = el})
-    this.interfaceservice.deleteCbox(this.choseItem).then(() => {
-  
-      this.seardata=this.seardata.filter((h)=>{h.int_service_num !==this.dedata.int_service_num})
-      console.log(this.seardata)
-  })
-  //this.getALLdata();
- // console.log(this.seardata)
-  this.seardata = this.seardata
-  //location.reload();
- 
-}
-/***
- * 
- * 
- * 
- * 分页
- */
-	public loadData(){  
-		let offset = (this.currentPage-1)*this.itemsPerPage;
-		let end = (this.currentPage)*this.itemsPerPage;
-    console.log(this.currentPage)
-   //跑了全部的数据
-   console.log(this.seardata)
-   if(this.flag1){
-            console.log(this.flag1)
-    return this.interfaceservice.getSdate().subscribe(
-        res=>{
-        this.interdatas=res['items'].slice(offset,end>this.totalItems?this.totalItems:end);
+  //这里是从后台获取删除后的数据，点击删除后的操作
+  deleteCC(cc: ofInterface[]){
+    console.log(this.choseItem)
+    this.interfaceservice.deleteCbox(this.choseItem).subscribe(res=>{
+      console.log(res)
+       this.deletdata = res['items']
+       console.log(this.deletdata)
+    }
+)    
+    console.log(this.deletdata)
+  }
+  /**
+  *新增   //刷新问题
+  */
+  addSave(addDatas: ofInterface): void {
+   
+    $(".waiting").show();
+    this.alertAdd = true;
+    console.log(this.alertAdd)
+    this.alertEdit = false;
+    this.alertSearch = false;
+    if (!addDatas) { return }
+    if (addDatas.URL == undefined) {
+      addDatas.URL = "";//新增的时候去除两个空的undefinded的数据
+    }
+    if (addDatas.remark == undefined) {
+      addDatas.remark = "";
+    }
+    this.interfaceservice.AddDatas(addDatas)
+      .then(ofInterface => { this.seardata.push(ofInterface); $(".waiting").hide();console.log(addDatas), console.log(this.interdatas) })
+  }
+  /**
+   * 编辑里面的更新事件
+   */
+  EditUpdatec(SSdatas: ofInterface, index: number) {
+    $(".waiting").show();
+    //console.log(SSdatas)
+    //this.indexw = index + 1.7976931348623157E+10308;//这里给一个无限的数字表示不管怎么递增都不能达到这个数字
+    this.interfaceservice.EditUpdate(SSdatas).subscribe(
+         
+      (h) => {
+          $(".waiting").hide();
+             this.deletdata = h['items']//确定这个是否存在，存在再换成this.seardata
+       console.log(this.deletdata)
+
+      }, error => {  $(".waiting").hide();alert("网络错误,请稍后重试！"); },
+      () => { }
+    )
+  }
+  /**
+   * 返回
+   */
+  goBack(): void {
+    this.location.back();
+  }
+  /**
+   * 表示编辑的时候input框出现
+   */
+  // editCon( aa: ofInterface,index:number,) {
+  //   this.indexw =index;
+  // }
+  /***
+   * 分页
+   */
+  public loadData() {
+    this.cancel();
+    $(".waiting").show();
+    let offset = (this.currentPage - 1) * this.itemsPerPage;
+    let end = (this.currentPage) * this.itemsPerPage;
+    //跑了全部的数据
+    if (this.flag1) {
+      console.log(this.flag1)
+      return this.interfaceservice.getSdate().subscribe(
+        res => {
+          this.interdatas = res['items'].slice(offset, end > this.totalItems ? this.totalItems : end);
+          $(".waiting").hide();
           console.log(this.interdatas)
         },
-        error => {console.log(error)},
-        () => {}
-      );  
-   }else if(this.flag3 && this.flag1){
+        error => { console.log(error) },
+        () => { }
+      );
+    } else if (this.flag3 && this.flag1) {
       console.log(this.flag1)
-    return this.interfaceservice.searchIn(this.jkfwdata).subscribe(
-            res=>{
-            this.interdatas=this.interdatas.slice(offset,end>this.totalItems?this.totalItems:end);
-              console.log(res)
-            },
-            error => {console.log(error)},
-            () => {}
-          );
-   }else{
-     console.log(this.flag1)
-    return this.interfaceservice.gadsearch(this.choosedata).subscribe(
-            res=>{
-            this.interdatas=this.interdatas.slice(offset,end>this.totalItems?this.totalItems:end);
-              console.log(res)
-            },
-            error => {console.log(error)},
-            () => {}
-          );
-   }	
-	}
+      return this.interfaceservice.searchIn(this.jkfwdata).subscribe(
+        res => {
+          this.interdatas = this.interdatas.slice(offset, end > this.totalItems ? this.totalItems : end);
+          $(".waiting").hide();
+          console.log(res)
+        },
+        error => { console.log(error) },
+        () => { }
+      );
+    } else {
+      console.log(this.flag1)
+      return this.interfaceservice.gadsearch(this.choosedata).subscribe(
+        res => {
+          this.interdatas = this.interdatas.slice(offset, end > this.totalItems ? this.totalItems : end);
+          $(".waiting").hide();
+          console.log(res)
+        },
+        error => { console.log(error) },
+        () => { }
+      );
+    }
+  }
   public pageChanged(event: any): void {
     this.currentPage = event.page;
     console.log(this.currentPage)
     console.log('Page changed to: ' + event.page);
-    console.log('Number items per page: ' + event.itemsPerPage);   
+    console.log('Number items per page: ' + event.itemsPerPage);
   }
 }
